afbeb2849ed935e1242bf7e99887f656156e0469
Devang1224 <97470914+Devang1224@users.noreply.github.com>
2026-02-12 12:55:47 +0530

    feat: add filters

--- src/hooks/useCameraTable.jsx
+++ src/hooks/useCameraTable.jsx
@@ -12,17 +12,41 @@ const useCameraTable = () => {
     search:'',
     page:1,
     limit:10,
-
   });
 
   
   
-const filteredData = useMemo(()=>{
-     if(tableData?.length > 0) {
-        return tableData;
+const { data: filteredData, totalFilteredCount } = useMemo(() => {
+     if (tableData?.length > 0) {
+        let totalPages = Math.ceil(tableData.length / filters.limit);
+
+        if (filters.page > totalPages) {
+          filters.page = totalPages;
+        }
+        if (filters.page < 1) {
+          filters.page = 1;
+        }
+
+        const filteredOnly = [];
+        tableData.forEach((item) => {
+          if (filters.location !== 'all' && item.location.toLowerCase() !== filters.location.toLowerCase()) {
+            return;
+          }
+          if (filters.status !== 'all' && item.status.toLowerCase() !== filters.status.toLowerCase()) {
+            return;
+          }
+          if (filters.search !== '' && !item.name.toLowerCase().includes(filters.search.toLowerCase())) {
+            return;
+          }
+          filteredOnly.push(item);
+        });
+
+        const total = filteredOnly.length;
+        const data = filteredOnly.slice((filters.page - 1) * filters.limit, filters.page * filters.limit);
+        return { data, totalFilteredCount: total };
      }
-     return [];
-},[tableData,filters]);
+     return { data: [], totalFilteredCount: 0 };
+}, [tableData, filters]);
 
 
   const getCameraData = async()=>{
@@ -53,9 +77,9 @@ const filteredData = useMemo(()=>{
         filters,
         isLoading,
         error,
-        filteredData
-
-    }  
+        filteredData,
+        totalFilteredCount,
+    };  
 }
 
 export default useCameraTable
--- src/pages/DashBoard.jsx
+++ src/pages/DashBoard.jsx
@@ -6,125 +6,174 @@ import { GoLocation } from "react-icons/go";
 import { FaWifi } from "react-icons/fa";
 import Table from "../components/ui/Table";
 import { IoBan } from "react-icons/io5";
+import { RiArrowLeftDoubleFill } from "react-icons/ri";
+import { RiArrowLeftSLine } from "react-icons/ri";
+import { MdKeyboardArrowRight } from "react-icons/md";
+import { MdKeyboardDoubleArrowRight } from "react-icons/md";
 
 const DashBoard = () => {
-  const { 
-    filteredData,
-    isLoading,
-    error,
-    setFilters,
-    filters } = useCameraTable();
+  const {
+     filteredData,
+     isLoading,
+     error,
+     setFilters,
+     filters,
+     totalFilteredCount } = useCameraTable();
 
-
-const columns = [
-  {
-    id:"name",
-    label:'Name',
-  },
-  {
-    id:"health",
-    label:'Health',
-  },
-  {
-    id:"location",
-    label:'Location',
-  },
-  {
-    id:"recorder",
-    label:'Recorder',
-  },
-  {
-    id:"tasks",
-    label:'Tasks',
-  },
-  {
-    id:"status",
-    label:'Status',
-  },
-  {
-    id:"actions",
-    label:'Actions',
-  },
-
-]
+  const columns = [
+    {
+      id: "name",
+      label: "Name",
+    },
+    {
+      id: "health",
+      label: "Health",
+    },
+    {
+      id: "location",
+      label: "Location",
+    },
+    {
+      id: "recorder",
+      label: "Recorder",
+    },
+    {
+      id: "tasks",
+      label: "Tasks",
+    },
+    {
+      id: "status",
+      label: "Status",
+    },
+    {
+      id: "actions",
+      label: "Actions",
+    },
+  ];
 
   return (
     <div className="dash">
-        <div className="dash-header">
-            <div className="dash-head">
-               <p className="dash-title">Cameras</p>
-               <p className="dash-desc">Manage your cameras here</p>
-            </div>
-            <div className="dash-search">
-                <input
-                  type="text"
-                  placeholder="search"
-
-                />
-                <button>
-                  <IoSearch className="dash-search-icon"/>
-                </button>
-            </div>
-
+      <div className="dash-header">
+        <div className="dash-head">
+          <p className="dash-title">Cameras</p>
+          <p className="dash-desc">Manage your cameras here</p>
         </div>
-
-        {/* filters  */}
-        <div className="dash-filters">
-          <div className="dash-select">
-            <GoLocation className="dash-select-icon"/>
-            <select>
-              <option value="all" selected disabled>Location</option>
-              <option value="active">Active</option>
-              <option value="inactive">Inactive</option>
-            </select>
-          </div>
-          <div className="dash-select">
-            <FaWifi className="dash-select-icon tilted"/>
-            <select>
-              <option value="all" selected disabled>Status</option>
-              <option value="active">Active</option>
-              <option value="inactive">Inactive</option>
-            </select>
-          </div>
+        <div className="dash-search">
+          <input
+            type="text"
+            placeholder="search"
+            onChange={(e) => setFilters({ ...filters, search: e.target.value })}
+            value={filters.search}
+          />
+          <button>
+            <IoSearch className="dash-search-icon" />
+          </button>
         </div>
-        
-        {/* table */}
-        <div className="dash-table">
-              <Table>
-                 <thead className="table-head">
-                     <th className="table-head-item">
-                      <input type="checkbox" className="table-checkbox" />
-                     </th>
-                     {
-                      columns.map((column)=>(
-                        <th className="table-head-item" key={column.id}>{column.label}</th>
-                      ))
-                     }
-                 </thead>
-                 <tbody>
-                    {
-                      filteredData.map((item,index)=>(
-                        <tr className="table-row" key={item.id}>
-                          <td className="table-cell">
-                           <input type="checkbox" className="table-checkbox"/>
-                          </td>
-                          <td className="table-cell">Camera {index+1}</td>
-                          <td className="table-cell">{item.health}</td>
-                          <td className="table-cell">{item.location}</td>
-                          <td className="table-cell">{item.name}</td>
-                          <td className="table-cell">3 Tasks</td>
-                          <td className="table-cell">{item.status}</td>
-                          <td className="table-cell ">
-                            <IoBan className="table-icon"/>
-                          </td>
-                        </tr>
-                      ))
-                    }
-                 </tbody>
-              </Table>
-        </div>
+      </div>
 
-   </div>
+      {/* filters  */}
+      <div className="dash-filters">
+        <div className="dash-select">
+          <GoLocation className="dash-select-icon" />
+          <select
+            onChange={(e) =>
+              setFilters({ ...filters, location: e.target.value })
+            }
+            value={filters.location}
+          >
+            <option value="all" selected>
+              Location
+            </option>
+            <option value="active">Active</option>
+            <option value="inactive">Inactive</option>
+          </select>
+        </div>
+        <div className="dash-select">
+          <FaWifi className="dash-select-icon tilted" />
+          <select
+            onChange={(e) => setFilters({ ...filters, status: e.target.value })}
+            value={filters.status}
+          >
+            <option value="all" selected>
+              Status
+            </option>
+            <option value="active">Active</option>
+            <option value="inactive">Inactive</option>
+          </select>
+        </div>
+      </div>
+
+      {/* table */}
+      <div className="dash-table">
+        <Table>
+          <thead className="table-head">
+            <th className="table-head-item">
+              <input type="checkbox" className="table-checkbox" />
+            </th>
+            {columns.map((column) => (
+              <th className="table-head-item" key={column.id}>
+                {column.label}
+              </th>
+            ))}
+          </thead>
+          <tbody>
+            {filteredData.map((item, index) => (
+              <tr className="table-row" key={item.id}>
+                <td className="table-cell">
+                  <input type="checkbox" className="table-checkbox" />
+                </td>
+                <td className="table-cell">Camera {index + 1}</td>
+                <td className="table-cell">{item.health}</td>
+                <td className="table-cell">{item.location}</td>
+                <td className="table-cell">{item.name}</td>
+                <td className="table-cell">3 Tasks</td>
+                <td className="table-cell">{item.status}</td>
+                <td className="table-cell ">
+                  <IoBan className="table-icon" />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </div>
+
+      {/* pagination */}
+      <div className="dash-pagination">
+        <select
+          onChange={(e) => setFilters({ ...filters, limit: e.target.value })}
+          value={filters.limit}
+        >
+          <option value="10" selected>
+            10
+          </option>
+          <option value="20">20</option>
+          <option value="30">30</option>
+          <option value="40">40</option>
+          <option value="50">50</option>
+        </select>
+
+        <p className="dash-pagination-count">
+          {totalFilteredCount === 0
+            ? "0 - 0 of 0"
+            : `${(filters.page - 1) * filters.limit + 1} - ${Math.min(filters.page * filters.limit, totalFilteredCount)} of ${totalFilteredCount}`}
+        </p>
+
+        <div className="dash-pagination-buttons">
+          <button>
+            <RiArrowLeftDoubleFill />
+          </button>
+          <button>
+            <RiArrowLeftSLine />
+          </button>
+          <button>
+            <MdKeyboardArrowRight />
+          </button>
+          <button>
+            <MdKeyboardDoubleArrowRight />
+          </button>
+        </div>
+      </div>
+    </div>
   );
 };
 
